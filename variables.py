2874b0da2c4659c6a36c0dec521646a2fe3a0c22
Otto <max.t@live.ru>
2024-11-19 20:34:13 +0300

    Восстановил ветку

---
+++ variables.py
@@ -0,0 +1,6 @@
+amount_successful_DZ = 12
+amount_hours = 1.5
+name_course = 'Python'
+time_one_task = amount_hours/amount_successful_DZ
+
+print(name_course+',', 'всего задач: ' + str(amount_successful_DZ), 'затрачено часов: ' + str(amount_hours), 'среднее время выполнения ' + str(time_one_task) + ' часа.')
