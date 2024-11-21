e7e93298cfb54dd8195dc9a542176321c8ae63c3
Otto <max.t@live.ru>
2024-11-21 23:19:09 +0300

    Практическая работа по уроку "Организация программ и методы строк"

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input('Введите текст: ')
+
+print('Количество символов:',len(my_string))
+print('Строка в верхнем регистре:',my_string.upper())
+print('Строка в нижнем регистре:',my_string.lower())
+print('В строке удалены пробелы:',my_string.replace(' ',''))
+print('Первый символ строки:',my_string[0])
+print('Последний символ строки:',my_string[-1])
